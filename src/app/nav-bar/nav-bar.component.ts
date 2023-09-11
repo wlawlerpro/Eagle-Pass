cf69a42b576b50c96d409ee13ac7e605e17fa933
Wes Lawler <weslawler@MacBook-Pro.local>
2023-09-11 19:51:49 -0400

    trying to input a toggle menu

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+isMenuOpen = false; 
 
+toggleMenu() {
+  this.isMenuOpen = !this.isMenuOpen; 
+}
 }
