3e0edb14a2e083ac70121bc808fc0fd19c476fdf
Wes Lawler <weslawler@MacBook-Pro.local>
2023-10-03 19:15:53 -0400

    added banner image

---
+++ src/app/hbanner-component/banner-component/banner-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-component',
+  templateUrl: './banner-component.component.html',
+  styleUrls: ['./banner-component.component.css']
+})
+export class BannerComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
