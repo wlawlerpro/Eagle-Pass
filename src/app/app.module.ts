c11e0aa01411e80edcacab2f918960482df3671e
Wes Lawler <weslawler@MacBook-Pro.local>
2023-10-12 19:24:32 -0400

    added footer needs work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BannerComponentComponent } from './hbanner-component/banner-component/banner-component.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    BannerComponentComponent
+    BannerComponentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
